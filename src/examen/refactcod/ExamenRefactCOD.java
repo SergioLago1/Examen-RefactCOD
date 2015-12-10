60d2ea65bc1f8f102411051c90712f8bc53ba794
slagogonzalez <slagogonzalez@atanasoff28.danielcastelao.org>
2015-12-10 09:13:25 +0100

    Cambio de nombre de variables

--- src/examen/refactcod/ExamenRefactCOD.java
+++ src/examen/refactcod/ExamenRefactCOD.java
@@ -4,7 +4,7 @@
  * and open the template in the editor.
  */
 package examen.refactcod;
-
+import javax.swing.JOptionPane;
 /**
  *
  * @author slagogonzalez
@@ -17,30 +17,31 @@ public class ExamenRefactCOD {
     public static boolean p = false;
 
     public static void main(String arg[]) {
-        int dig = 3;
+        
+       
         int ndig = 0;
-        if (dig <= 0) {
+        if (numero <= 0) {
             System.out.println("Ingrese como parámetro, un numero de digitos correcto (mayor que 0): ");
         }
         for (int i = 1; i <= 99999; i++) {
             int aux = i;
 
-            int contador = 0;
+            int contadorNum = 0;
 
             while (aux != 0) {
                 aux = aux / 10;
-                contador++;
+                contadorNum++;
             }
-            ndig = contador;
+            ndig = contadorNum;
 
-            if (ndig == dig) {
+            if (ndig == numero) {
                 if (i < 4) {
                     p = true;
                 } else {
                     if (i % 2 == 0) {
                         p = false;
                     } else {
-                        int contador1 = 0;
+                        int contadorNum1 = 0;
                         int i1 = 1;
                         int k = (i - 1) / 2;
                         if (k % 2 == 0) {
@@ -49,15 +50,15 @@ public class ExamenRefactCOD {
 
                         while (i1 <= k) {
                             if (i % i1 == 0) {
-                                contador1++;
+                                contadorNum1++;
                             }
                             i1 += 2;
-                            if (contador1 == 2) {
+                            if (contadorNum1 == 2) {
                                 i1 = k + 1;
                             }
                         }
 
-                        if (contador1 == 1) {
+                        if (contadorNum1 == 1) {
                             p = true;
                         }
                     }
